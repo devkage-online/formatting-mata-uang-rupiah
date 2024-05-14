17e6d4f67616c02baf436a3c2a467f19459d1c8f
devkage.online <info@devkage.online>
2024-05-14 20:44:18 +0700

    add examples

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Globalization;
+
+namespace FormattingIdr
+{
+    static class Program
+    {
+        static void Main()
+        {
+            // Set CultureInfo ke Indonesia
+            CultureInfo indonesiaCulture = new("id-ID");
+
+            // Jumlah uang
+            double amount = 7250050.35;
+
+            // Format C2 - angka dibelakang koma akan menjadi sen
+            string formattedAmountC2 = amount.ToString("C2", indonesiaCulture);
+            string formattedAmountC0 = amount.ToString("C0", indonesiaCulture);
+
+            // Output
+            Console.WriteLine(formattedAmountC2); // Output: Rp7.250.050
+            Console.WriteLine(formattedAmountC0); // Output: Rp7.250.050,35 
+        }
+    }
+}
